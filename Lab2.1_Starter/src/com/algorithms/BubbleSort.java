2e149dc6b5a997c1a321b92b4c93b37927347a40
Michael Herman <89651714+MichaelBHerman@users.noreply.github.com>
2022-06-01 13:03:14 -0600

    lab 2.1 bubble sorting completed

--- Lab2.1_Starter/src/com/algorithms/BubbleSort.java
+++ Lab2.1_Starter/src/com/algorithms/BubbleSort.java
@@ -18,8 +18,23 @@ bubbleSort(array)
      if(array[j] > array[j + 1])
        swap(array, j, j + 1)
 */
-    public static void sort(int[] numbers){
-    // implement pseudo-code above
+public static void sort(int[] arrayToBeSorted){
+
+    int n = arrayToBeSorted.length;
+
+    int count = 0;
+
+    for (int k = 1; k < n; k++){
+        for (int j = 0; j < arrayToBeSorted.length-1; j++){
+            count++; //counting each iteration of the array
+            if (arrayToBeSorted[j] > arrayToBeSorted[j+1]){
+                int tempIndexValue = arrayToBeSorted[j]; //placeholder for the greater index value
+                arrayToBeSorted[j] = arrayToBeSorted[j+1];  //swapping greater value with next index
+                arrayToBeSorted[j+1] = tempIndexValue; //swapping lesser value with greater index
+            }
+        }
     }
+    System.out.println("Iterations for array of length " + arrayToBeSorted.length + ": " + count );
+}
     
 }
